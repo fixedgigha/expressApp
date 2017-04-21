da65752720a41384f73e99680833657bbd7006b3
Jim Speakman <jim.speakman@cybg.com>
2017-04-21 16:43:00 +0100

    Auth header

--- routes/oauth2.js
+++ routes/oauth2.js
@@ -4,11 +4,13 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/authorize', function(req, res, next) {
+    const auth = req.get("authorization");
+    const token = auth ? auth.split(" ").pop() : undefined;
     const responseType = req.query.response_type;
     const clientId = req.query.client_id;
     const redirectUri = req.query.redirect_uri;
 
-    console.log(`Authorize client ${clientId} type ${responseType}`);
+    console.log(`Authorize bearer ${token} client ${clientId} type ${responseType}`);
     res.redirect(`${redirectUri}?code=87654321`);
 });
 
